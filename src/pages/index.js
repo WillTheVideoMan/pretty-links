790c6a0bce7fbd264e52f2fcb8505de0208a1a98
Will Hall <accounts@willhall.uk>
2020-05-09 16:05:47 +0100

    💩 Added hacky colourful glowing splash screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useRef } from "react"
+import SimplexNoise from "simplex-noise"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const App = () => {
+  const canvasRef = useRef(null)
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  let t = 0
 
-export default IndexPage
+  const simplex = new SimplexNoise()
+
+  const handleClick = e => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext("2d")
+    const imageData = ctx.createImageData(50, 50)
+
+    for (let x = 0; x < 50; x++) {
+      for (let y = 0; y < 50; y++) {
+        const i = (x + y * 50) * 4
+        const r = Math.abs(simplex.noise3D(x / 400, y / 400, t / 500) * 255)
+        const g = Math.abs(simplex.noise3D(x / 400, y / 400, t / 500 + 1) * 255)
+        const b = Math.abs(simplex.noise3D(x / 400, y / 400, t / 500 + 2) * 255)
+        imageData.data[i] = r
+        imageData.data[i + 1] = g
+        imageData.data[i + 2] = b
+        imageData.data[i + 3] = 255
+      }
+    }
+    ctx.putImageData(imageData, 0, 0)
+
+    t++
+
+    window.requestAnimationFrame(handleClick)
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{
+        margin: "0",
+        padding: "0",
+        width: window.innerWidth + "px",
+        height: window.innerHeight + "px",
+      }}
+      width="50px"
+      height="50px"
+      onClick={handleClick}
+    />
+  )
+}
+
+export default App
