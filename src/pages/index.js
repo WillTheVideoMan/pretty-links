fa3ec3a620c89e73d14244667c8d2408dea3df6c
Will Hall <accounts@willhall.uk>
2020-05-10 13:28:38 +0100

    💄 Add custom bi-tone colour. Add text w/ blend.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
-import React, { useRef } from "react"
+import React, { useRef, useState, useEffect } from "react"
 import SimplexNoise from "simplex-noise"
 
 const App = () => {
   const canvasRef = useRef(null)
 
+  const [dimensions, setDimensions] = useState([0, 0])
+
+  useEffect(() => {
+    setDimensions([window.innerWidth, window.innerHeight])
+    draw()
+  }, [])
+
   let t = 0
 
   const simplex = new SimplexNoise()
 
-  const handleClick = e => {
+  const lerp = (x, x1, x2, y1, y2) => y1 + (x - x1) * ((y2 - y1) / (x2 - x1))
+
+  const palette = [
+    [255, 0, 0],
+    [0, 255, 255],
+  ]
+
+  const draw = e => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext("2d")
     const imageData = ctx.createImageData(50, 50)
@@ -16,12 +30,11 @@ const App = () => {
     for (let x = 0; x < 50; x++) {
       for (let y = 0; y < 50; y++) {
         const i = (x + y * 50) * 4
-        const r = Math.abs(simplex.noise3D(x / 400, y / 400, t / 500) * 255)
-        const g = Math.abs(simplex.noise3D(x / 400, y / 400, t / 500 + 1) * 255)
-        const b = Math.abs(simplex.noise3D(x / 400, y / 400, t / 500 + 2) * 255)
-        imageData.data[i] = r
-        imageData.data[i + 1] = g
-        imageData.data[i + 2] = b
+        const n = simplex.noise3D(x / 400, y / 400, t / 400)
+        const nMag = Math.abs(n)
+        imageData.data[i] = lerp(nMag, 0, 1, palette[0][0], palette[1][0])
+        imageData.data[i + 1] = lerp(nMag, 0, 1, palette[0][1], palette[1][1])
+        imageData.data[i + 2] = lerp(nMag, 0, 1, palette[0][2], palette[1][2])
         imageData.data[i + 3] = 255
       }
     }
@@ -29,22 +42,39 @@ const App = () => {
 
     t++
 
-    window.requestAnimationFrame(handleClick)
+    window.requestAnimationFrame(draw)
   }
 
   return (
-    <canvas
-      ref={canvasRef}
-      style={{
-        margin: "0",
-        padding: "0",
-        width: "500px",
-        height: "500px",
-      }}
-      width="50px"
-      height="50px"
-      onClick={handleClick}
-    />
+    <div>
+      <h1
+        style={{
+          zIndex: "99",
+          position: "absolute",
+          mixBlendMode: "difference",
+          color: "white",
+          fontFamily: "mono",
+          left: "24px",
+        }}
+      >
+        Amazing Text Magic!
+      </h1>
+      <canvas
+        ref={canvasRef}
+        style={{
+          margin: "0",
+          padding: "0",
+          width: dimensions[0] + "px",
+          height: dimensions[1] + "px",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          zIndex: "0",
+        }}
+        width="50px"
+        height="50px"
+      />
+    </div>
   )
 }
 
