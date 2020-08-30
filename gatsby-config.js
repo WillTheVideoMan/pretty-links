c8a1b42bbeb3865d723c33be15e8a29aacfc2177
Will Hall <hello@willhall.uk>
2020-08-30 23:49:27 +0100

    📈 Add Ackee.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,21 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-json`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `@willthevideoman/gatsby-plugin-ackee-tracker`,
+      options: {
+        domainId: process.env.ACKEE_DOMAIN_ID,
+        server: process.env.ACKEE_SERVER,
+        ignoreLocalhost: true,
+        detailed: true,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
